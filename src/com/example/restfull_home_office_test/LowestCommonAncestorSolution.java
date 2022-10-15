ecbad05451c7c5fc7c1336273792d02982996deb
Mariana Stoiovici <mirela.stoiovici@ada.ac.uk>
2022-10-15 14:04:21 +0100

    Add problem statement.

--- src/com/example/restfull_home_office_test/LowestCommonAncestorSolution.java
+++ src/com/example/restfull_home_office_test/LowestCommonAncestorSolution.java
@@ -1,7 +1,36 @@
 package com.example.restfull_home_office_test;
-
-import java.util.HashMap;
-
+//Find the first (lowest) common ancestor of two nodes in a binary tree without allocating any tree data structure in memory.
+//
+//The first common ancestor of two nodes v and w is the lowest (i.e. deepest) node that has both v and w as descendants.
+//
+//Consider the binary tree's indexes starting from 1 in the root, increasing from the leftmost node to the right at each level. (Standard tree node indexing from left to right)
+//
+//INPUT
+//int    index1
+//int    index2
+//
+//OUTPUT
+//int    indexCommonAncestor
+//
+//CONSTRAINTS
+//1 <= index1 <= 1000000
+//1 <= index2 <= 1000000
+//
+//EXAMPLES
+//Input
+//13, 15
+//Output
+//3
+//
+//Input
+//11, 13
+//Output
+//1
+//
+//Input
+//10, 11
+//Output
+//5
 public class LowestCommonAncestorSolution {
     // Time and Space log(index1) or log(index2) whichever is larger. Space being used because of the stack calls as
     //this is a recursive solution
@@ -32,40 +61,24 @@ public class LowestCommonAncestorSolution {
     }
 
     public static void main(String[] args) {
-//        int sol = run2(13,15);
-//        System.out.println("sol : " + sol);
-//        int sol1 = run2(11,13);
-//        System.out.println("sol1 : " + sol1);
-//        int sol2 = run2(10,11);
-//        System.out.println("sol2 : " + sol2);
-//        int sol3 = run2(1,1);
-//        System.out.println("sol3 : " + sol3);
-//        int sol4 = run2(2,1000000);
-//        System.out.println("sol4 : " + sol4);
-//        int sol5 = run2(9,25);
-//        System.out.println("sol5 : " + sol5);
-//        int sol6 = run2(61,62);
-//        System.out.println("sol6 : " + sol6);
-//        int sol7 = run2(2,3);
-//        System.out.println("sol7 : " + sol7);
+//        System.out.println("sol : " + run2(13,15));
+//        System.out.println("sol1 : " + run2(11,13));
+//        System.out.println("sol2 : " + run2(10,11));
+//        System.out.println("sol3 : " + run2(1,1));
+//        System.out.println("sol4 : " + run2(2,1000000));
+//        System.out.println("sol5 : " + run2(9,25));
+//        System.out.println("sol6 : " + run2(61,62));
+//        System.out.println("sol7 : " + run2(2,3));
 
 
-        int sol = run(13,15);
-        System.out.println("sol : " + sol);
-        int sol1 = run(11,13);
-        System.out.println("sol1 : " + sol1);
-        int sol2 = run(10,11);
-        System.out.println("sol2 : " + sol2);
-        int sol3 = run(1,1);
-        System.out.println("sol3 : " + sol3);
-        int sol4 = run(2,1000000);
-        System.out.println("sol4 : " + sol4);
-        int sol5 = run(9,25);
-        System.out.println("sol5 : " + sol5);
-        int sol6 = run(61,62);
-        System.out.println("sol6 : " + sol6);
-        int sol7 = run(2,3);
-        System.out.println("sol7 : " + sol7);
+        System.out.println("sol : " + run(13,15));
+        System.out.println("sol1 : " + run(11,13));
+        System.out.println("sol2 : " + run(10,11));
+        System.out.println("sol3 : " + run(1,1));
+        System.out.println("sol4 : " + run(2,1000000));
+        System.out.println("sol5 : " + run(9,25));
+        System.out.println("sol6 : " + run(61,62));
+        System.out.println("sol7 : " + run(2,3));
 
 
     }
