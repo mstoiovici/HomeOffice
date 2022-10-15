36d129423f77aa13946386171fde884c825d648e
Mariana Stoiovici <mirela.stoiovici@ada.ac.uk>
2022-10-15 14:05:39 +0100

    Fix statement.

--- src/com/example/restfull_home_office_test/LowestCommonAncestorSolution.java
+++ src/com/example/restfull_home_office_test/LowestCommonAncestorSolution.java
@@ -1,9 +1,11 @@
 package com.example.restfull_home_office_test;
-//Find the first (lowest) common ancestor of two nodes in a binary tree without allocating any tree data structure in memory.
+//Find the first (lowest) common ancestor of two nodes in a binary tree without allocating any tree data structure in
+// memory.
 //
 //The first common ancestor of two nodes v and w is the lowest (i.e. deepest) node that has both v and w as descendants.
 //
-//Consider the binary tree's indexes starting from 1 in the root, increasing from the leftmost node to the right at each level. (Standard tree node indexing from left to right)
+//Consider the binary tree's indexes starting from 1 in the root, increasing from the leftmost node to the right at
+// each level. (Standard tree node indexing from left to right)
 //
 //INPUT
 //int    index1
